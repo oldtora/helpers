8722da8ec2992a3dee10a9238b6c8b7a662485ca
oldtora <50335753+oldtora@users.noreply.github.com>
2022-05-06 00:40:52 +0300

    Create install_docker.sh

---
+++ tools/install_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install wget -y
+wget -O get-docker.sh https://get.docker.com
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+rm -f get-docker.sh
+sudo usermod -aG docker $USER
