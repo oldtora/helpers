9bf501405e494262eb47df92785b8fdd4b536090
oldtora <50335753+oldtora@users.noreply.github.com>
2022-07-27 22:59:14 +0300

    Update install.sh

--- node_exporter/install.sh
+++ node_exporter/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "-----------------------------------------------------------------------------"
+
 curl -s https://raw.githubusercontent.com/oldtora/helpers/main/oldtora.sh | bash
-echo "-----------------------------------------------------------------------------"
 sleep 5
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.0/node_exporter-1.2.0.linux-amd64.tar.gz && \
 tar xvf node_exporter-1.2.0.linux-amd64.tar.gz && \
@@ -10,9 +9,7 @@ sudo mv node_exporter-1.2.0.linux-amd64 node_exporter && \
 chmod +x $HOME/node_exporter/node_exporter && \
 mv $HOME/node_exporter/node_exporter /usr/bin && \
 rm -Rvf $HOME/node_exporter/
-echo "-----------------------------------------------------------------------------"
 echo "Чёто сделали"
-echo "-----------------------------------------------------------------------------"
 sleep 5
 sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
 [Unit]
@@ -32,7 +29,5 @@ sudo systemctl daemon-reload && \
 sudo systemctl enable exporterd && \
 sudo systemctl restart exporterd
 sleep 2
-echo "-----------------------------------------------------------------------------"
 echo "Сделали чёто ещё"
 echo "Проверяем сделанное: journalctl -u exporterd -f"
-echo "-----------------------------------------------------------------------------"
